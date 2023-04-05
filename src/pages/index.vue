5bc3a766c576a692828489c52de9285cac9ab56a
June <zkitun@163.com>
2023-04-05 21:52:04 +0800

    项目初始化

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Logos />
+    <Examples />
+    <h1>第一个页面</h1>
+    <NuxtLink to="/list">进入列表页</NuxtLink>
+    <br />
+    <br />
+    <NuxtLink to="/user-admin/111">进入管理员 id 为 111 的用户中心</NuxtLink>
+  </div>
+</template>
---
+++ src/pages/list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ListItem />
+    <div v-focus>focus</div>
+    <strong> 姓名：{{ userInfo.userName }} 性别：{{ userInfo.sex }} </strong>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/stores/useUserStore'
+
+const { userInfo } = useUserStore()
+// console.log(userInfo);
+</script>
